59ebdab03cb71043fe8b0bfce4a5b43680e1d985
Theo Henson <34148527+tteeoo@users.noreply.github.com>
2022-01-27 13:22:05 +0000

    Remove mouse movement (too glitchy, unnecessary)

--- client/script/player.js
+++ client/script/player.js
@@ -1,7 +1,5 @@
 // This file defines the player object.
 
-// TODO: refactor player
-
 // Initialize a localController object.
 function localPlayer(remotePlayerID) {
 	this.rotation = 0;
@@ -9,36 +7,11 @@ function localPlayer(remotePlayerID) {
 
 function controller() {
 	this.rotation = 0;
-	this.targetX = 0;
-	this.targetY = 0;
 	this.speedX = 0;
 	this.speedY = 0;
-	this.followMouse = false;
 }
 
 function updateController() {
-	// Stop if following mouse and at cursor.
-	if (me.c.followMouse) {
-		if (me.c.speedX > 0) {
-			if (me.p.pos[0] + me.c.speedX * 4 >= me.c.targetX) {
-				me.c.speedX = 0;
-			}
-		} else {
-			if (me.p.pos[0] + me.c.speedX * 4 <= me.c.targetX) {
-				me.c.speedX = 0;
-			}
-		}
-		if (me.c.speedY > 0) {
-			if (me.p.pos[1] + me.c.speedY * 4 >= me.c.targetY) {
-				me.c.speedY = 0;
-			}
-		} else {
-			if (me.p.pos[1] + me.c.speedY * 4 <= me.c.targetY) {
-				me.c.speedY = 0;
-			}
-		}
-	}
-
 	me.c.rotation = drawPlayer(me.p.pos[0], me.p.pos[1], me.c.rotation, me.p.mass, me.p.angVel, me.p.name, "black");
 }
 
